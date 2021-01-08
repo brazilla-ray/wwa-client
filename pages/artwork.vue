bd7339a4deca3534205e36a0c1ac72d580985616
William Whitaker <billydwhitaker@gmail.com>
2021-01-07 19:20:10 -0500

    adds basic logic to artwork page

--- pages/artwork.vue
+++ pages/artwork.vue
@@ -1,5 +1,37 @@
 <template>
-  <section class="section">
-    <NuxtLink to="/">home</NuxtLink>
-  </section>
+  <div class="wrapper">
+    <section class="section">
+      <NuxtLink to="/">home</NuxtLink>
+    </section>
+    <section class="section">
+      <div class="artwork">
+        <div v-for="artwork in artworks" :key="artwork.id">
+          <figure>
+            <img :srcset="artwork.image.srcset" />
+            <figcaption>
+              <p>{{ artwork.title }}</p>
+              <p>{{ artwork.date }}</p>
+              <p>{{ artwork.medium }}</p>
+              <p>{{ artwork.dimensions }}</p>
+            </figcaption>
+          </figure>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      selectedTag: null,
+    }
+  },
+  computed: {
+    artworks() {
+      return this.$store.state.artworks
+    },
+  },
+}
+</script>
