b9e3b57bce919b555fd0cded14fc0dfa7f9ebb0a
William Whitaker <billydwhitaker@gmail.com>
2021-01-09 17:56:33 -0500

    adds allTags array

--- pages/artwork.vue
+++ pages/artwork.vue
@@ -5,6 +5,13 @@
     </section>
     <section class="section">
       <div class="artwork">
+        <div class="tags"></div>
+        <h2>tags:</h2>
+        <ul>
+          <li v-for="tag in allTags" :key="tag.id">
+            {{ tag }}
+          </li>
+        </ul>
         <div v-for="artwork in artworks" :key="artwork.id">
           <figure>
             <img :srcset="artwork.image.srcset" />
@@ -26,11 +33,23 @@ export default {
   data() {
     return {
       selectedTag: null,
+      sizeTags: ['5x4', '10x7', '14x10', '24x18', '38x25'],
     }
   },
   computed: {
     artworks() {
       return this.$store.state.artworks
+    },
+    tags() {
+      return this.$store.state.tags
+    },
+    allTags() {
+      const tagList = []
+      this.tags.forEach((el) => {
+        tagList.push(el.tags)
+      })
+      const allTags = [...new Set(tagList.flat())]
+      return allTags.sort()
     },
   },
 }
