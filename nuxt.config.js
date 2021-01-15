2f00c980b6641059ed759be17a198a88d2d58b58
William Whitaker <billydwhitaker@gmail.com>
2021-01-15 06:56:06 -0500

    adds fontawesome

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,12 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: ['normalize.css', 'bulma', '@/assets/css/main.scss'],
+  css: [
+    'normalize.css',
+    'bulma',
+    '@fortawesome/fontawesome-free',
+    '@/assets/css/main.scss',
+  ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
