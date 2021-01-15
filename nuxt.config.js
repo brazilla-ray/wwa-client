565dd37c102b2c86f39e179df8b6cd9382c1b257
William Whitaker <billydwhitaker@gmail.com>
2021-01-15 06:33:57 -0500

    adds normalize.css

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: ['bulma', '@/assets/css/main.scss'],
+  css: ['normalize.css', 'bulma', '@/assets/css/main.scss'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
