d693c24c9118115da5b4190a27fd7b97446df16f
William Whitaker <billydwhitaker@gmail.com>
2021-01-19 20:20:31 -0500

    gets tags working properly

--- components/ArtworkTags.vue
+++ components/ArtworkTags.vue
@@ -3,11 +3,15 @@
     <h2 class="mb-2">Type:</h2>
     <ul class="tags">
       <li
-        v-for="type in types"
-        :key="type.id"
+        v-for="tag in mediaTypes"
+        :key="tag.id"
+        :class="[
+          tag === selectedTag ? 'has-background-link has-text-link-light' : '',
+        ]"
         class="tag is-light is-medium is-link"
+        @click="updateTag(tag)"
       >
-        {{ type.slug }}
+        {{ tag.slug }}
       </li>
     </ul>
   </div>
@@ -17,6 +21,7 @@
 export default {
   data() {
     return {
+      selectedTag: null,
       typeTags: ['painting', 'drawing', 'collage'],
     }
   },
@@ -24,8 +29,17 @@ export default {
     tags() {
       return this.$store.state.tags
     },
-    types() {
+    mediaTypes() {
       return this.tags.filter((el) => this.typeTags.includes(el.slug)).reverse()
+    },
+  },
+  methods: {
+    updateTag(tag) {
+      if (!this.selectedTag) {
+        this.selectedTag = tag
+      } else {
+        this.selectedTag = null
+      }
     },
   },
 }
