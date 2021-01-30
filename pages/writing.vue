380d79b2d0073df1cb2901d6112ebeb9805039c9
William Whitaker <billydwhitaker@gmail.com>
2021-01-30 15:08:37 -0500

    shows posts on 'writing' page

--- pages/writing.vue
+++ pages/writing.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="wrapper">
     <page-header-vue></page-header-vue>
+    <div class="posts">
+      <main>
+        <h2>posts</h2>
+        <div v-for="post in posts" :key="post.id" class="post">
+          <h3>
+            <a :href="`${post.slug}`">{{ post.title.rendered }}</a>
+          </h3>
+          <div v-html="post.content.rendered"></div>
+          <a :href="`blog/${post.slug}`">Read more =></a>
+        </div>
+      </main>
+    </div>
   </div>
 </template>
 
@@ -9,6 +21,14 @@ import PageHeaderVue from '~/components/PageHeader.vue'
 export default {
   components: {
     PageHeaderVue,
+  },
+  computed: {
+    posts() {
+      return this.$store.state.posts
+    },
+  },
+  created() {
+    this.$store.dispatch('getPosts')
   },
 }
 </script>
