7434ae5f560e32f42ec4d0a8c2562fdb3a251aaf
William Whitaker <billydwhitaker@gmail.com>
2021-01-31 19:59:25 -0500

    adds hero to 'writing' page

--- pages/writing.vue
+++ pages/writing.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="wrapper">
     <page-header-vue></page-header-vue>
+    <hero-vue hero-title="writing"></hero-vue>
     <div class="posts">
       <main>
-        <h2>posts</h2>
         <div v-for="post in posts" :key="post.id" class="post">
           <h3>
             <a :href="`${post.slug}`">{{ post.title.rendered }}</a>
@@ -18,9 +18,11 @@
 
 <script>
 import PageHeaderVue from '~/components/PageHeader.vue'
+import HeroVue from '~/components/Hero.vue'
 export default {
   components: {
     PageHeaderVue,
+    HeroVue,
   },
   computed: {
     posts() {
