32b6a361eb8abd5542612fb35aa1c90f8276f1de
William Whitaker <billydwhitaker@gmail.com>
2021-02-02 14:49:40 -0500

    stubs out contact form

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,14 +1,48 @@
 <template>
   <div class="wrapper">
     <page-header-vue></page-header-vue>
+    <hero-vue hero-title="contact"></hero-vue>
+    <section class="mt-6 section">
+      <form name="contact" action="" method="post">
+        <div class="field">
+          <label class="form-label" for="name"> Name: </label>
+          <div class="control">
+            <input id="name" class="input" type="text" name="name" />
+          </div>
+        </div>
+        <div class="field">
+          <label class="form-label" for="email"> Email: </label>
+          <div class="control">
+            <input id="email" class="input" type="text" name="email" />
+          </div>
+        </div>
+        <div class="field">
+          <label class="form-label" for="message"> Message: </label>
+          <div class="control">
+            <textarea id="message" class="textarea" name="message"></textarea>
+          </div>
+        </div>
+        <div class="field is-grouped">
+          <div class="control">
+            <button class="button is-link">Submit</button>
+          </div>
+          <div class="control">
+            <button class="button is-link is-light">Cancel</button>
+          </div>
+        </div>
+      </form>
+    </section>
   </div>
 </template>
 
 <script>
 import PageHeaderVue from '~/components/PageHeader.vue'
+import HeroVue from '~/components/Hero.vue'
+
 export default {
   components: {
     PageHeaderVue,
+    HeroVue,
   },
 }
 </script>
