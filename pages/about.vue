5381024f1e1bfa3c9330f745817d64b36e2e4335
William Whitaker <billydwhitaker@gmail.com>
2021-02-01 20:23:04 -0500

    adds hero to about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="wrapper">
     <page-header-vue></page-header-vue>
+    <hero-vue hero-title="about"></hero-vue>
   </div>
 </template>
 
 <script>
 import PageHeaderVue from '~/components/PageHeader.vue'
+import HeroVue from '~/components/Hero.vue'
 export default {
   components: {
     PageHeaderVue,
+    HeroVue,
   },
 }
 </script>
